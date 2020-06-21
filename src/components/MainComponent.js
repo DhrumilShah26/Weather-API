a629bfd095036c4c353dca858c3c24f4b7689ee0
Dhrumil <sdhrumil2605@gmail.com>
2020-06-20 20:35:33 -0400

    initial release

---
+++ src/components/MainComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Home from './HomeComponent';
+import Header from './HeaderComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>   
+        <Switch>
+            <Route path="/home" component={() => <Home/>} />
+            <Redirect to="/home" />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Main;
